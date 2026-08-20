2559820dee99983e12cbace0c0d00d3c8dae14c0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-20 18:23:17 +0530

    Traversal an Arrays Using Enhanced

--- Star.java
+++ Star.java
@@ -1,11 +1,11 @@
 public class Star{
     public static void main(String args[]){
-        int [] arr = {10, 20, 30, 40, 50};
+        int [] arr = {10, 20, 35, 40, 5};
 
         System.out.println("Arrays elements: - " );
 
-        for(int i = 0; i<arr.length; i++){
-            System.out.println(arr[i]);
+        for(int num : arr){
+            System.out.print(num + " ");
         }
     }
 }
