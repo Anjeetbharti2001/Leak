1bbea97cc1661344738d28d87c175db575ee121b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-23 10:52:17 +0530

    java byte data type

--- Star.java
+++ Star.java
@@ -1,6 +1,7 @@
 public class Star{
     public static void main(String args[]){
-        boolean flag = true;
-        System.out.println(flag);
+       byte range;
+       range = 124;
+       System.out.println(range);
     }
 }
