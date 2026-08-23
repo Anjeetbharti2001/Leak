6366efd589c12251526fee850fca06c352667f5d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-23 10:49:47 +0530

    boolean Data print true

--- Star.java
+++ Star.java
@@ -1,12 +1,6 @@
-public class Star {
+public class Star{
     public static void main(String args[]){
-        double myDouble = 3.4;
-        float myFloat = 3.4f;
-
-        Double myDoubleScientific = 3.44e2;
-
-        System.out.println(myDouble);
-        System.out.println(myFloat);
-        System.out.println(myDoubleScientific);
+        boolean flag = true;
+        System.out.println(flag);
     }
 }
