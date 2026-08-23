c153d56198846457bd4b926cdf0f8af91e63e977
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-23 10:46:29 +0530

    Floating pointing literals

--- Star.java
+++ Star.java
@@ -1,6 +1,12 @@
-public class Star{
+public class Star {
     public static void main(String args[]){
-        System.out.println("Anjeet Bharti ");
-        
+        double myDouble = 3.4;
+        float myFloat = 3.4f;
+
+        Double myDoubleScientific = 3.44e2;
+
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myDoubleScientific);
     }
 }
