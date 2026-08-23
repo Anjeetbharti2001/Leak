a52acfc9b13e6f3d7b9ed861aace78d8d90ed57b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-23 10:40:18 +0530

    first program running

--- Star.java
+++ Star.java
@@ -1,15 +1,6 @@
 public class Star{
-    static int sumArray(int[] arr, int n){
-        if(n == 0)
-            return arr[0];
-
-        return arr[n] + sumArray(arr, n - 1);
-    }
     public static void main(String args[]){
-        int [] arr = {1, 2, 3, 4, 5};
-        int sum = sumArray(arr, arr.length - 1);
-
-        System.out.println("Sum of the elements int the arrays:-" + sum);
-
+        System.out.println("Anjeet Bharti ");
+        
     }
 }
