90a02421addccf53bfcc17bbd89954009a693fdc
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-24 09:10:21 +0530

    Enhanced an arrays (for-each loop)

--- Star.java
+++ Star.java
@@ -1,10 +1,10 @@
 public class Star{
     public static void main(String args[]){
-        int [] arr = {4,7,9,0,4,5,5};
-        System.out.print("Arrays element :-");
+        int [] arr = {6,8,9,5,7,3,6};
+        System.out.println("Element of arrays:-");
 
-        for(int i = 0; i < 7;  i++){
-            System.out.print(arr[i] + " ");
+        for(int num : arr){
+            System.out.print(num + " ");
         }
     }
 }
