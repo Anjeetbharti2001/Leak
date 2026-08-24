508e7224a706f10a816acb1359b9ad26809617c5
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-24 09:26:02 +0530

    Reverse arrays in java

--- Star.java
+++ Star.java
@@ -1,10 +1,10 @@
-public class Star{
+public class Star {
     public static void main(String args[]){
-        int [] arr = {6,8,9,5,7,3,6};
-        System.out.println("Element of arrays:-");
+        int arr[] = {2, 6, 8, 7, 6, 9};
+        System.out.println("Reverse arrays in java element:-");
 
-        for(int num : arr){
-            System.out.print(num + " ");
+        for(int i = arr.length - 1; i>= 0; i--){
+            System.out.print(arr[i] + " ");
         }
     }
 }
