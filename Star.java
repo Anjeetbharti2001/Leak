a619b74ed9245e29e656f76332c344a98d382e56
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-24 08:56:47 +0530

    Traversal Arrays in java

--- Star.java
+++ Star.java
@@ -1,7 +1,10 @@
 public class Star{
     public static void main(String args[]){
-       byte range;
-       range = 124;
-       System.out.println(range);
+        int [] arr = {4,7,9,0,4,5,5};
+        System.out.print("Arrays element :-");
+
+        for(int i = 0; i < 7;  i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
