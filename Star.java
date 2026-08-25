c3880c581ee24804a9f957263517ee1b87488da3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-25 17:40:12 +0530

    Integer value

--- Star.java
+++ Star.java
@@ -1,23 +1,6 @@
-import java.util.*;
-public class Star{
+public class Star {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of Arrays:- ");
-
-        int n = sc.nextInt();
-
-        int [] arr = new int[n];
-
-        System.out.println("Enter arrys elements:-");
-
-        for(int i = 0; i<n ; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Arrays elements :-");
-
-        for(int i = 0; i<n; i++){
-            System.out.print(arr[i] + " ");
-        }
-
+       int myNum = 5;
+       System.out.println(myNum); 
     }
 }
