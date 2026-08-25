68a228253f0ac62fbe698c53855d6ed9c0d78f89
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-25 17:46:38 +0530

    Character value

--- Star.java
+++ Star.java
@@ -1,6 +1,6 @@
 public class Star {
     public static void main(String args[]){
-       float  myFloatNum = 5.99f;
-       System.out.println(myFloatNum); 
+       char  myLetter = 'D';
+       System.out.println(myLetter); 
     }
 }
