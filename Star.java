87ba07d83545d01d2be8150d2eb18fbf87107493
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-25 17:43:33 +0530

    Floating poit number

--- Star.java
+++ Star.java
@@ -1,6 +1,6 @@
 public class Star {
     public static void main(String args[]){
-       int myNum = 5;
-       System.out.println(myNum); 
+       float  myFloatNum = 5.99f;
+       System.out.println(myFloatNum); 
     }
 }
