57dc1ef8aa63db438a4c84a4194d0f250a119896
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-26 09:18:29 +0530

    short data type in java

--- Star.java
+++ Star.java
@@ -1,8 +1,8 @@
-public class Star {
-    public static void main(String arrgs[]){
-        byte age = 25;
-        byte temperature = -10;
-        System.out.println("Age:" + age);
-        System.out.println("Temperature:" + temperature);
+public class Star{
+    public static void main(String args[]){
+        short students = 1000;
+        short temp = -200;
+        System.out.println("Number of Students :" + students);
+        System.out.println("Temperature: " + temp);
     }
 }
