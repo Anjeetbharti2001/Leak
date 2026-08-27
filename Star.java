407397cc055599a1ee71c8cfcdf1428c14908b79
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-27 09:36:07 +0530

    Enhanced loop traversal an Arrays

--- Star.java
+++ Star.java
@@ -1,24 +1,9 @@
-import java.util.Scanner;
-
 public class Star{
     public static void main(String args[]){
-        
-        Scanner sc = new Scanner(System.in);
+        int arr[] = {4,7,8,9,4,2};
 
-        System.out.print("Enter size of arrays:-");
-
-        int n = sc.nextInt();
-        
-        int [] arr = new int[n];
-
-        System.out.print("Enter arrays element:- ");
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.print("Total of arrays element:-");
-
-        for(int i = 0; i<n ; i++){
-            System.out.print(arr[i] + " ");
+        for(int num : arr){
+            System.out.print(num + " ");
         }
     }
 }
