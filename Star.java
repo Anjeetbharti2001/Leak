f3a15441d5e0f193aab45d6fe3a18bb2f11c80e8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-27 09:26:31 +0530

    user through input and traversal an arrays

--- Star.java
+++ Star.java
@@ -1,8 +1,24 @@
-public class Star {
+import java.util.Scanner;
+
+public class Star{
     public static void main(String args[]){
-        float pi = 3.14f;
-        float gravity = 9.81f;
-        System.out.println("Pi :-" + pi);
-        System.out.println("Gravity :" + gravity);
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of arrays:-");
+
+        int n = sc.nextInt();
+        
+        int [] arr = new int[n];
+
+        System.out.print("Enter arrays element:- ");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Total of arrays element:-");
+
+        for(int i = 0; i<n ; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
