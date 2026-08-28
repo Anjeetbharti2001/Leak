e2919bb7a149ae0c143008f2a3ac39d2b39c3fef
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-28 10:17:01 +0530

    Sort arrays accending order

--- Star.java
+++ Star.java
@@ -1,27 +1,10 @@
+import java.util.*;
 public class Star{
-    static int BinarySearch(int arr[], int target){
-        int left = 0;
-        int right = arr.length -1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(arr[mid] == target){
-                return mid;
-            }else if(arr[mid] < target){
-                left = mid;
-            }else if(arr[mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid -1;
-            }
-        }
-        return -1;
-    }
     public static void main(String args[]){
-        int arr[] = {10, 20, 30, 40, 50, 60, 70};
-        int target = 50;
+        int arr[] = {4,7,9,2,6,5};
 
-        int result = BinarySearch(arr, target);
-        System.out.println("Found element this :" + result);
+        Arrays.sort(arr);
 
+        System.out.println(Arrays.toString(arr));
     }
 }
