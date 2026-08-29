40dc45367d498776166dcc3bf6f892b34039868f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-29 07:55:10 +0530

    Selection Sort

--- Star.java
+++ Star.java
@@ -1,14 +1,17 @@
 public class Star{
     public static void main(String args[]){
-        int arr[] = {5,2,8,1,9};
+        int [] arr = {5,2,8,1};
 
-        for(int i = 0; i<arr.length - 1; i++){
-            for(int j = 0; j<arr.length - i - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j + 1] = temp;
-                }
+        for(int i = 0; i<arr.length-1; i++){
+            int minIndex = i;
+
+            for(int j = i; j<arr.length; j++){
+                if(arr[i] <arr[minIndex]);
+                minIndex = j;
             }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
         for(int num : arr){
             System.out.print(num + " ");
