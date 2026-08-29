592beac0d7f7e71a9dcd38fdf147b6e7aaa3c438
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-29 08:10:13 +0530

    Insertion Sort

--- Star.java
+++ Star.java
@@ -1,20 +1,41 @@
-public class Star{
-    public static void main(String args[]){
-        int [] arr = {5,2,8,1};
+// public class Star{
+//     public static void main(String args[]){
+//         int [] arr = {5,2,8,1};
 
-        for(int i = 0; i<arr.length-1; i++){
-            int minIndex = i;
+//         int key = arr[i];
 
-            for(int j = i; j<arr.length; j++){
-                if(arr[i] <arr[minIndex]);
-                minIndex = j;
-            }
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-        for(int num : arr){
-            System.out.print(num + " ");
-        }
-    }
+//         int j = i - 1;
+//         while (j >= 0 && arr[j] > key){
+//             arr[j + 1] = arr[i];
+//             j--;
+//         }
+//         arr[j + 1] = key;
+//     }
+//     for(int num : arr){
+//         System.out.println(num + " ");
+//     }
+// }
+
+public class Star { 
+    public static void main(String[] args) { 
+        int[] arr = {5, 2, 8, 1}; 
+
+        // Fixed: Added the outer loop to iterate through the array
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i]; 
+            int j = i - 1; 
+
+            // Shifts elements that are greater than the key
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; // Fixed: Changed '==' to '=' and 'arr[i]' to 'arr[j]'
+                j--; 
+            } 
+            arr[j + 1] = key; 
+        } 
+
+        // Prints the sorted array
+        for (int num : arr) { 
+            System.out.print(num + " "); // Fixed: Changed println to print for a single line
+        } 
+    } 
 }
