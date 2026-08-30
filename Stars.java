d1dd4623c521110bc567eb73020a45ce81b8a2b8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-30 17:23:45 +0530

    Super with Constructors

--- Stars.java
+++ Stars.java
@@ -1,24 +1,18 @@
+class Person{
+    Person(){
+        System.out.println("Person class Constructor");
+    }
+}
+
+class Student extends Person{
+    Student(){
+        super();
+
+        System.out.println("Stident class Constructor");
+    }
+}
 public class Stars{
-
-    int a ;
-    int b;
-
-    // Default constructor 
-    Stars(){
-        this(10, 20){
-            System.out.println("Inside default constructor \n");
-        }
-    }
-
-    // Prameterized constructor
-    Stars(int a, int b){
-        this.a = a;
-        this.b = b;
-
-        System.out.println("Inside parameterized constructor");
-    }
-    
     public static void main(String args[]){
-        Stars object = new Stars();
+        Student s = new Student();
     }
 }
