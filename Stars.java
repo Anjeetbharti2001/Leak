3a3aa33ae4fbdefc2f4d4bca3aa466b5cde47a26
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-08-31 15:40:33 +0530

    Linear Search on java

--- Stars.java
+++ Stars.java
@@ -1,15 +1,21 @@
 public class Stars{
     public static void main(String args[]){
-        int arr [] = {5,10,15,20};
+        int arr[] = {3,5, 5, 6, 59, 6};
 
-        int [] prefix = new int[arr.length];
+        int key = 23;
 
-        prefix[0] = arr[0];
+        int index = -1;
 
-        for(int i = 1; i<arr.length; i++){
-            prefix[i] = prefix[i - 1] + arr[i];
-
-            System.out.println("Total sum = " + prefix[arr.length - 1]);
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == key){
+                index = i;
+                break;
+            }
+        }
+        if(index != -1){
+            System.out.println("Element found at index !" + index);
+        }else{
+            System.out.println("Element is not found:");
         }
     }
 }
