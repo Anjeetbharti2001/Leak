153e7476ad3ac4f86a5c3e5253ac66c3ff83d913
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-01 10:01:55 +0530

    find the first accurences

--- Stars.java
+++ Stars.java
@@ -1,13 +1,14 @@
 public class Stars{
     public static void main(String args[]){
-        int [] arr = {2,4,2,6,2,8};
-        int key = 2;
-        
-        int count = 0;
-        for(int num : arr){
-            count++;
+        int [] arr = {3, 5, 7, 5, 9};
+
+        int key = 5;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == key){
+                System.out.println(i);
+                break;
+            }
         }
-         System.out.println(count);
     }
-   
 }
