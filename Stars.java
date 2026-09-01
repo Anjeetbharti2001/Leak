b09b0d5ef8dae270ba8c2d4eab3b9d2570a928af
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-01 09:35:05 +0530

    Find an element in an arrays

--- Stars.java
+++ Stars.java
@@ -1,20 +1,17 @@
 public class Stars{
-    static int LinearSearch(int [] arr, int key){
-         for(int i = 0; i<arr.length; i++){
-            if(arr[i] == key){
-                return i;
-            }
-         }
-         return -1;
-    }
     public static void main(String args[]){
-        int [] arr = {5, 10, 15, 20, 25};
+        int [] arr = {10, 20, 30, 40, 50};
 
-        int result = LinearSearch(arr, 20);
-        if(result != -1){
-            System.out.println("found at index:");
-        }else{
-            System.out.println("Not found");
+        int key = 30;
+
+        int index = -1;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == key){
+                index = i;
+                break;
+            }
         }
+        System.out.println(index);
     }
 }
