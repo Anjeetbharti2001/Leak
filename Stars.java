8620cf0e0ded7b0a5e09fc2590995b91ca34e34b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-02 08:28:44 +0530

    find the minimum element in arrays

--- Stars.java
+++ Stars.java
@@ -2,13 +2,13 @@ public class Stars{
     public static void main(String args[]){
         int [] arr = {5,7,83,2,4,5,6};
         
-        int max = arr[0];
+        int min = arr[0];
 
         for(int num : arr){
-            if(num > max){
-                max = num;
+            if(num < min){
+                min = num;
             }
         }
-        System.out.println(max);
+        System.out.println( "minimum element:-" + min);
     }
 }
