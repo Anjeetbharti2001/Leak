af6716786872e0d6508a6b7eec51ccf8c11e7601
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-02 08:35:23 +0530

    find the all Indices of an elements

--- Stars.java
+++ Stars.java
@@ -1,14 +1,12 @@
 public class Stars{
     public static void main(String args[]){
         int [] arr = {5,7,83,2,4,5,6};
-        
-        int min = arr[0];
-
-        for(int num : arr){
-            if(num < min){
-                min = num;
+    
+        int key = 2;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == key){
+                System.out.print(i + " ");
             }
         }
-        System.out.println( "minimum element:-" + min);
     }
 }
