44db5793c272398509b62de7eec9030edfd1ff0b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-02 08:21:17 +0530

    find the maximum element in arrays

--- Stars.java
+++ Stars.java
@@ -1,14 +1,14 @@
 public class Stars{
     public static void main(String args[]){
-        int [] arr = {3, 5, 7, 5, 9};
+        int [] arr = {5,7,83,2,4,5,6};
+        
+        int max = arr[0];
 
-        int key = 5;
-
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] == key){
-                System.out.println(i);
-                break;
+        for(int num : arr){
+            if(num > max){
+                max = num;
             }
         }
+        System.out.println(max);
     }
 }
