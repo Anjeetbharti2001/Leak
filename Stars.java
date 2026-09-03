de35cfa19429ffed9eb7311d0a993fe36512f0ba
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-03 10:52:12 +0530

    block-scope - local Variable

--- Stars.java
+++ Stars.java
@@ -1,6 +1,11 @@
 public class Stars{
     public static void main(String args[]){
-        int x = 10;
-        System.out.println(x);
+
+        int x;
+        for( x = 0; x < 4; x++){
+            System.out.println(x);
+        }
+        // will produces erroer
+        System.out.println(x + " ");
     }
 }
