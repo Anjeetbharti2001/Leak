f0fab8414996e167c4216b51e107ea451aaf6408
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-03 10:41:59 +0530

    Method and parametere Scope

--- Stars.java
+++ Stars.java
@@ -1,11 +1,21 @@
-import java.io.*;
-
-class Test{
-    // static variable in Test class 
-    static int var = 10;
-}
 public class Stars{
+
+    static int x = 11;
+    private int y = 23;
+
+    public void testFunc(int x){
+        Stars t = new Stars();
+        this.x = 22;
+
+        y = 44;
+
+        System.out.println("Stars.x : " + Stars.x);
+        System.out.println("T.x" + t.x);
+        System.out.println("t.x " + t.x);
+        System.out.println("t.y" + y);
+    }
     public static void main(String args[]){
-        System.out.println("Static variable : " + Test.var);
+       Stars t = new Stars();
+       t.testFunc(5);
     }
 }
