0a717c3f22ac420fd0d0cdb4d1600a12c564a47e
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-03 10:44:33 +0530

    Block lavel Scopes

--- Stars.java
+++ Stars.java
@@ -1,21 +1,6 @@
 public class Stars{
-
-    static int x = 11;
-    private int y = 23;
-
-    public void testFunc(int x){
-        Stars t = new Stars();
-        this.x = 22;
-
-        y = 44;
-
-        System.out.println("Stars.x : " + Stars.x);
-        System.out.println("T.x" + t.x);
-        System.out.println("t.x " + t.x);
-        System.out.println("t.y" + y);
-    }
     public static void main(String args[]){
-       Stars t = new Stars();
-       t.testFunc(5);
+        int x = 10;
+        System.out.println(x);
     }
 }
