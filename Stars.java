0712f534feb0bc9211ae1889490b97ff82687a27
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-04 09:26:20 +0530

    Removes Duplicates Arrays

--- Stars.java
+++ Stars.java
@@ -1,23 +1,18 @@
-public class Stars{
-    public static void findPair(int [] arr, int target){
-        int left = 0;
-        int right = arr.length -1;
+ public class Stars{
+    public static int removeDuplicates(int [] arr){
+        int i = 0;
 
-        while(left < right){
-            int sum = arr[left] + arr[right];
-            if(sum == target){
-                System.out.println(arr[left] + " " + arr[right]);
-
-                return ;
-            }else if(sum < target){
-                left++;
-            }else{
-                right--;
+        for(int j = 0; j<arr.length; j++){
+            if(arr[i] != arr[j]){
+                j++;
+                arr[i] = arr[j];
             }
         }
+        return i + 1;
     }
     public static void main(String args[]){
-        int [] arr = {1,2,3,4,5,6};
-        findPair(arr, 6);
+        int [] arr = {1,1,2,2,3,4,4};
+
+        System.out.println(removeDuplicates(arr));
     }
-}
+ }
