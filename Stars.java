f35ce0f2b842849ebfb4ec2c8623050dd62f3766
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-04 08:53:55 +0530

    check if palindrome yes or no

--- Stars.java
+++ Stars.java
@@ -1,31 +1,19 @@
-// Reverse an array using two pointers
-
-public class Stars {
-
-    public static void reverse(int[] arr) {
-
+public class Stars{
+    public static boolean isPalindrome(int [] arr){
         int left = 0;
-        int right = arr.length - 1;
+        int right = arr.length -1;
 
-        while (left < right) {
-
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-
+        while(left < right){
+            if(arr[left] != arr[right]){
+                return false;
+            }
             left++;
             right--;
         }
-
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        return true;
     }
-
-    public static void main(String args[]) {
-
-        int arr[] = {3, 5, 6, 7, 8, 9};
-
-        reverse(arr);
+    public static void main(String args[]){
+        int [] arr = {1,2,3,2,1};
+        System.out.println(isPalindrome(arr));
     }
 }
