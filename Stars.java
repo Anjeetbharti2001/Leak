0a4a22f1cabf978416bbee781f75e205130c3689
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-05 10:41:30 +0530

    Relational Operators

--- Stars.java
+++ Stars.java
@@ -1,28 +1,15 @@
 public class Stars{
     public static void main(String args[]){
-        int [] arr = {-7, -3, -1, 4, 8, 10};
+        //Comparison Operators
+        int a = 10;
+        int b = 3;
+        int c = 5;
 
-        int n = arr.length;
-
-        int [] result = new int[n];
-        int left = 0;
-        int right = n - 1;
-
-        for(int i = 0; i>= 0; i--){
-            int leftSquare = arr[left] * arr[left];
-            int rightSquare = arr[right] * arr[right];
-
-            if(leftSquare > rightSquare){
-                result[i] = leftSquare;
-                left++;
-            }else{
-                result[i] = rightSquare;
-                right--;
-            }
-        }
-        System.out.print("Sorted square :-");
-        for(int num : result){
-            System.out.print(num + " ");
-        }
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b :" + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b :" + (a <= b ));
+        System.out.println("a == c :" + (a == c));
+        System.out.println("a != c :" + (a != c));
     }
 }
