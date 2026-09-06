2403d797aa202daf9b8f1c02348f496b6ae61da2
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-06 09:37:38 +0530

    An - static integer used to save memory in large numeric arrrays

--- Stars.java
+++ Stars.java
@@ -1,9 +1,9 @@
 public class Stars{
     public static void main(String args[]){
-        boolean isJavaFun = true;
-        boolean isFishTasty = false;
-        
-        System.out.println("Is Java Fun? " + isJavaFun);
-        System.out.println("Is fish tasty?" + isFishTasty);
+        byte age = 25;
+        byte temperature = -10;
+
+        System.out.println("Age :" + age);
+        System.out.println("Temperature:-" + temperature);
     }
 }
