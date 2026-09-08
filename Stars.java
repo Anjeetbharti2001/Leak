af275f65f7b392e425b9faffb197e9f3619f9e0f
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-08 10:14:42 +0530

    Successful demonstration

--- Stars.java
+++ Stars.java
@@ -1,9 +1,11 @@
 public class Stars{
-    public static void main(String args[]){
-        int [] numbers = {1,2,3,4,5};
-        String [] names = {"Anjeet", "Bharti", " Dear Anjeet"};
+    public static void  main(String args[]){
+       int x = 10;
 
-        System.out.println("First number :" + numbers[0]);
-        System.out.println("Second name :" + names[1]);
+        if(x > 10){
+            System.out.println("Failed");
+        }else{
+            System.out.println("Successful demonstration " + "of keywords.");
+        }
     }
 }
