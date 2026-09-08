ac4e492a5f5da6ee5b708b6790a9f4fe8bcd0c18
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-08 10:23:06 +0530

    Fixed-size collection of similar data types stored contiguously, ideal when element count is known.

--- Stars.java
+++ Stars.java
@@ -1,11 +1,16 @@
-public class Stars{
-    public static void  main(String args[]){
-       int x = 10;
+import java.util.*;
 
-        if(x > 10){
-            System.out.println("Failed");
-        }else{
-            System.out.println("Successful demonstration " + "of keywords.");
-        }
+public class Stars{
+    public static void main(String args[]){
+        // Arrays example 
+        int [] arr = {10, 20, 30, 40, 50};
+        System.out.println(Arrays.toString(arr));
+
+        // ArrayList Example
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
     }
 }
