14ff8b360460bf1e79db06f6bbb5126584de86ac
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-10 09:14:22 +0530

    Multiply by 2 *=

--- Stars.java
+++ Stars.java
@@ -6,7 +6,12 @@ public class Stars{
         // System.out.println("Initial : " + num);
 
         // add 5 -> num = num + 5
-        num += 5;
-        System.out.println("After +5 :" + num);
+        // num += 5;
+        // System.out.println("After +5 :" + num);
+
+        //multiplication 
+        num *= 2;
+        System.out.println("after * 2 :" + num);
+
     }
 }
