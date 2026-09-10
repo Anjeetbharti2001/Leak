ec559937d87ba212d46c631e318c5f5a78828cdd
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-10 09:05:53 +0530

    Initial value

--- Stars.java
+++ Stars.java
@@ -1,15 +1,8 @@
 public class Stars{
     public static void main(String args[]){
-        //Integer declared
-        int a = 10;
-        int b = 10;
-
-        // Using Unary Operators
-        // System.out.println("PostIncreament :-" + (a++));
-        // System.out.println("PreIncreament:-" + (++a));
-
-        System.out.println("PostDecreament :-" + (b--));
-        System.out.println("Predecreament:-" + (--b));
-
+        // Initial value
+        
+        int num = 10;
+        System.out.println("Initial : " + num);
     }
 }
