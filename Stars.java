87cf17b23c0ed82997bf9fc3ab450735040d8861
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-11 11:55:00 +0530

    shif bitwise operators

--- Stars.java
+++ Stars.java
@@ -4,6 +4,7 @@ public class Stars{
        int d = 0b1010;
        int e = 0b1100;
 
-       System.out.println("d & e : " + (d & e));
+    //    System.out.println("d & e : " + (d & e));
+    System.out.println("d | e : " + (d | e));
     }
 }
