5fd2e9d901fe34d8a60c496df6709832e6473811
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-12 10:08:57 +0530

    if else statement in java

--- Stars.java
+++ Stars.java
@@ -1,8 +1,11 @@
 public class Stars{
     public static void main(String args[]){
-        int i = 10;
-        if(i < 15){
-            System.out.println("Condition is True");
-        }
+       int i = 7;
+
+       if(i < 15){
+        System.out.println("I is Smaller than 15");
+       }else{
+        System.out.println("I is greater than 15");
+       }
     }
 }
