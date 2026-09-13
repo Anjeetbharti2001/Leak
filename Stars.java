6aeb321e3dbd68a6a38e2ad22783fcf156ca1ac4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-13 09:49:07 +0530

    condition is true

--- Stars.java
+++ Stars.java
@@ -1,6 +1,6 @@
 public class Stars{
     public static void main(String args[]){
-        int  i = 10;
+        int  i = 3;
 
         if(i < 5){
             System.out.println("condition is true");
