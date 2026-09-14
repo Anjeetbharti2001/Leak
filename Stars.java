653e1a1971a5c1fa948d5f5ee98e54b961cbeb7a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-14 09:36:18 +0530

    The switch statement is a multiway branch statement

--- Stars.java
+++ Stars.java
@@ -1,15 +1,22 @@
 public class Stars{
     public static void main(String args[]){
-        int i = 20;
+        int num = 20;
 
-        if(i == 10){
-            System.out.println("I is 10");
-        }else if(i == 15){
-            System.out.println("I is 15");
-        }else if(i == 20){
-            System.out.println("I is 20");
-        }else{
-            System.out.println("I is not present");
+        switch(num){
+            case 5:
+                System.out.println("It is 5");
+                break;
+                case 10:
+                    System.out.println("It is 10");
+                    break;
+                    case 15:
+                    System.out.println("I is 15");
+                    break;
+                    case 20:
+                        System.out.println("I is 20");
+                        break;
+                        default:
+                            System.out.println("Not present");
         }
     }
 }
