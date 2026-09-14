00674e4dcfd9a01b7774fa32a1abc1c15d1bedad
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-14 09:39:26 +0530

    condition ? expression_if_true : expression_if_false;

--- Stars.java
+++ Stars.java
@@ -1,22 +1,8 @@
 public class Stars{
     public static void main(String args[]){
-        int num = 20;
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
 
-        switch(num){
-            case 5:
-                System.out.println("It is 5");
-                break;
-                case 10:
-                    System.out.println("It is 10");
-                    break;
-                    case 15:
-                    System.out.println("I is 15");
-                    break;
-                    case 20:
-                        System.out.println("I is 20");
-                        break;
-                        default:
-                            System.out.println("Not present");
-        }
+        System.out.println("Maximum is " + max);
     }
 }
