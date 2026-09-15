6c5be87da3f5cb0ae2501a62ef0c139d6d393589
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-15 09:06:00 +0530

    Calling Methods in Different Ways

--- Stars.java
+++ Stars.java
@@ -1,9 +1,14 @@
-public class Stars{
-    void hello(){
-        System.out.println("This is a user-defined method.");
+abstract class StarsHelp{
+    abstract void check(String name); // abstract method
+}
+public class Stars extends StarsHelp{
+    @Override 
+    void check(String name){
+        System.out.println(name);
     }
+    
     public static void main(String args[]){
-        Stars obj = new Stars(); // Create object
-        obj.hello(); 
+        Stars obj = new Stars();//subclass object
+        obj.check("StarsForStars");
     }
 }
