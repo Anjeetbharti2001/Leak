75648a18a20ba9c77550a3fff37e6c07b286a877
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-15 09:10:37 +0530

    Calling the Predefined Methods

--- Stars.java
+++ Stars.java
@@ -1,14 +1,6 @@
-abstract class StarsHelp{
-    abstract void check(String name); // abstract method
-}
-public class Stars extends StarsHelp{
-    @Override 
-    void check(String name){
-        System.out.println(name);
-    }
-    
+public class Stars{
     public static void main(String args[]){
-        Stars obj = new Stars();//subclass object
-        obj.check("StarsForStars");
+        Stars obj = new Stars();
+        System.out.println(obj.hashCode() + " ");// Predefined Method
     }
 }
