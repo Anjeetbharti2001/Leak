23f0fd4153fe93f132472ac67a20277a660429c0
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-15 08:52:01 +0530

    call Stack method

--- Stars.java
+++ Stars.java
@@ -1,8 +1,29 @@
 public class Stars{
-    public static void main(String args[]){
-        int a = 10, b = 20;
-        int max = (a > b ) ? a : b;
+    public static void D(){
+        float D = 40.5f;
+        System.out.println("In method D ");
+    }
 
-        System.out.println("maximum is :- " + max);
+    public static void C(){
+        double C = 30.5;
+        System.out.println("In method C ");
+    }
+    
+     public static void B(){
+        int B = 20;
+        C();// Calling c 
+        System.out.println("In Method B");
+
+     }
+     
+     public static void A() {
+        int A = 10;
+        B(); // Calling B
+        System.out.println("In Method A ");
+     }
+
+    public static void main(String args[]){
+        A();// Stars with function A
+        D();// Then call D
     }
 }
