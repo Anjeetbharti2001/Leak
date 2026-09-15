9e0aaba3a6de26ad03478429734a28abb8d36e7d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-15 08:56:36 +0530

    Calling a User-Defined Method

--- Stars.java
+++ Stars.java
@@ -1,29 +1,9 @@
 public class Stars{
-    public static void D(){
-        float D = 40.5f;
-        System.out.println("In method D ");
+    void hello(){
+        System.out.println("This is a user-defined method.");
     }
-
-    public static void C(){
-        double C = 30.5;
-        System.out.println("In method C ");
-    }
-    
-     public static void B(){
-        int B = 20;
-        C();// Calling c 
-        System.out.println("In Method B");
-
-     }
-     
-     public static void A() {
-        int A = 10;
-        B(); // Calling B
-        System.out.println("In Method A ");
-     }
-
     public static void main(String args[]){
-        A();// Stars with function A
-        D();// Then call D
+        Stars obj = new Stars(); // Create object
+        obj.hello(); 
     }
 }
