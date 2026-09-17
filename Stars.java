39bd7a468610e538d8bd7f57264dcca5d5a0c0c3
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-17 07:53:10 +0530

    calling instance method

--- Stars.java
+++ Stars.java
@@ -1,139 +1,16 @@
-// abstract class Shape{
-//     String color;
-//     // these are abstract methods
-//     abstract double area();
-//     public abstract String toString();
-
-//     // abstract class can have the constractor
-//     public Shape(String color){
-//         System.out.println("Shape constructor called");
-//         this.color  = color;
-//     }
-
-//     // this is a concreate method
-//     public String getColor(){
-//         return color;
-//     }
-// }
-// class Circle extends Shape{
-//     double radius;
-//     public Circle(String color, double radius){
-//         // calling Shape constructor 
-//         super(color);
-//         System.out.println("Circle constructor called");
-//         this.radius = radius;
-//     }
-//     @Override double are(){
-//         return Math.PI * pow(radius, 2);
-//     }
-//     @Override public String toString(){
-//         return "Circle color is " + super.getColor() + "and area is :" + area(); 
-//     }
-// }
-// class Rectangle extends Shape{
-//     double length;
-//     double width;
-
-//     public Rectangle(String color, double length, double width){
-//         // calling shape constructor
-//         super(color);
-//         System.out.println("Rectangle constructor called");
-//         this.length = length;
-//         this.width = width;
-//     }
-
-//     @Override double area(){ return length * width; }
-
-//     @Override public String toString(){
-//         return "Rectangle color is" + super.getColor() + "and area is :" + area();
-//     }
-// }
-
-// public class Stars{
-//     public static void main(String args[]){
-//        Shape s1 = new Circle("Red", 2.2);
-//        Shape s2 = new Rectangle("Yellow", 2, 4);
-
-//        System.out.println(s1.toString());
-//        System.out.println(s2.toString());
-//     }
-// }
-
-abstract class Shape {
-    String color;
-
-    // Abstract methods
-    abstract double area();
-
-    public abstract String toString();
-
-    // Abstract class can have a constructor
-    public Shape(String color) {
-        System.out.println("Shape constructor called");
-        this.color = color;
-    }
-
-    // Concrete method
-    public String getColor() {
-        return color;
-    }
-}
-
-class Circle extends Shape {
-    double radius;
-
-    public Circle(String color, double radius) {
-        // Calling Shape constructor
-        super(color);
-
-        System.out.println("Circle constructor called");
-        this.radius = radius;
-    }
-
-    @Override
-    double area() {
-        return Math.PI * radius * radius;
-    }
-
-    @Override
-    public String toString() {
-        return "Circle color is " + super.getColor()
-                + " and area is : " + area();
-    }
-}
-
-class Rectangle extends Shape {
-    double length;
-    double width;
-
-    public Rectangle(String color, double length, double width) {
-        // Calling Shape constructor
-        super(color);
-
-        System.out.println("Rectangle constructor called");
-        this.length = length;
-        this.width = width;
-    }
-
-    @Override
-    double area() {
-        return length * width;
-    }
-
-    @Override
-    public String toString() {
-        return "Rectangle color is " + super.getColor()
-                + " and area is : " + area();
-    }
-}
-
 public class Stars {
+
+    String name = "Anjeet";
+
+    // Instance Method
+    void greet() {
+        System.out.println("Hello, " + name + "!");
+    }
+
     public static void main(String args[]) {
 
-        Shape s1 = new Circle("Red", 2.2);
-        Shape s2 = new Rectangle("Yellow", 2, 4);
+        Stars p = new Stars();
 
-        System.out.println(s1.toString());
-        System.out.println(s2.toString());
+        p.greet(); // Calling instance method
     }
 }
