85880b931d11e3f00c50c6692446abdd291c0e5b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-17 08:24:51 +0530

    Mutator Methods (Setters)

--- Stars.java
+++ Stars.java
@@ -1,13 +1,17 @@
 public class Stars{
     private int balance = 100;
 
-    // Accessor method(getter)
+    // Mutator method(setter)
+    public void setBalance(int amount){
+        balance += amount;
+    }
     public int getBalance(){
         return balance;
     }
     public static void main(String args[]){
-       Stars acc = new Stars();
-       System.out.println(
-        "balance : " + acc.getBalance());
+        Stars acc = new Stars();// Updating balance
+        acc.setBalance(50);
+        System.out.println(
+            "Balance :" + acc.getBalance());
     }
 }
