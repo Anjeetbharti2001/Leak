03f3d0a412472a27cee451e73cc282f1ff3c7069
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-17 08:01:43 +0530

    calling instance method by passing value

--- Stars.java
+++ Stars.java
@@ -1,16 +1,19 @@
-public class Stars {
+public class Stars{
+    public static void main(String args[]){
+        // creating object 
+        Stars obj = new Stars();
 
-    String name = "Anjeet";
-
-    // Instance Method
-    void greet() {
-        System.out.println("Hello, " + name + "!");
+        // calling instance method by passing value
+        obj.add(2,3);
+        System.out.println("Stars!");
     }
+    // Instance method with parameter
+    void add(int a, int b){
+        // local variables
+        int x = a;
+        int y = b;
+        int z = x + y;
 
-    public static void main(String args[]) {
-
-        Stars p = new Stars();
-
-        p.greet(); // Calling instance method
+        System.out.println("Sum : " + z);
     }
 }
