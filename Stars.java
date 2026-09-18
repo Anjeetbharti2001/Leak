0a7380874b4f69b8fd0b9f0849620658a6f60261
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-18 07:33:23 +0530

    charactor variable

--- Stars.java
+++ Stars.java
@@ -1,10 +1,7 @@
 public class Stars{
     public static void main(String args[]){
-        //Integer variable 
-        // int age = 25;
-        // System.out.println(age);
-        
-        // floating - point variable 
-        float price = 99.99f;
-        System.out.println(price);
-}}
+        // character variable
+        char grade = 'A';
+        System.out.println(grade);
+    }
+}
