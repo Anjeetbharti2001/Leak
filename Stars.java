0f55db85364b688fa68fea38b75bb621d92f5507
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-18 07:25:44 +0530

    Integer variable

--- Stars.java
+++ Stars.java
@@ -1,17 +1,7 @@
 public class Stars{
-    private int balance = 100;
-
-    // Mutator method(setter)
-    public void setBalance(int amount){
-        balance += amount;
-    }
-    public int getBalance(){
-        return balance;
-    }
     public static void main(String args[]){
-        Stars acc = new Stars();// Updating balance
-        acc.setBalance(50);
-        System.out.println(
-            "Balance :" + acc.getBalance());
+        //Integer variable 
+        int age = 25;
+        System.out.println(age);
     }
 }
