253ba53deac7242301ba058d49c984bf8b36fdf9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-18 07:30:07 +0530

    Floating pointing variable

--- Stars.java
+++ Stars.java
@@ -1,7 +1,10 @@
 public class Stars{
     public static void main(String args[]){
         //Integer variable 
-        int age = 25;
-        System.out.println(age);
-    }
-}
+        // int age = 25;
+        // System.out.println(age);
+        
+        // floating - point variable 
+        float price = 99.99f;
+        System.out.println(price);
+}}
