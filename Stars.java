a61ac382384d835c2618eeff5f282b5d631320d9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-18 07:44:13 +0530

    Boolean variable

--- Stars.java
+++ Stars.java
@@ -1,7 +1,7 @@
 public class Stars{
     public static void main(String args[]){
-        // character variable
-        char grade = 'A';
-        System.out.println(grade);
+        // character variable 
+        boolean isJavaFun = true;
+        System.out.println("Is java fun? " + isJavaFun);
     }
 }
