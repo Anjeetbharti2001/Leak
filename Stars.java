0f284a0a5a2da9fc32aaa4b2db158e44989dbc6d
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-19 10:11:18 +0530

    Loops are used for performing the same task multiple times.

--- Stars.java
+++ Stars.java
@@ -1,15 +1,8 @@
 public class Stars{
     public static void main(String args[]){
-        int i = 1;
-        if(i == 10 || i <  15){
-            // fIRST IF statement 
-            if(i < 15){
-                System.out.println("I is smaller than 15");
-            }if(i < 12){
-                System.out.println("I is samller than 12 too");
-            }else{
-                System.out.println("I is greater than 15");
-            }
+        int n = 5;
+        for(int i = 1; i<= n; i++){
+            System.out.println(i);
         }
     }
 }
