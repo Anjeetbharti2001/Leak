23e513088eed909168d4f2bebf3cf52ba8c0f06c
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-19 10:05:01 +0530

    Nested if else

--- Stars.java
+++ Stars.java
@@ -1,13 +1,15 @@
 public class Stars{
     public static void main(String args[]){
-        int a = 1, b = 2;
-
-        if(a < b){
-            System.out.println(b);
-        }else if(a > b){
-            System.out.println(a);
-        }else{
-            System.out.println(a + "==" + b);
+        int i = 1;
+        if(i == 10 || i <  15){
+            // fIRST IF statement 
+            if(i < 15){
+                System.out.println("I is smaller than 15");
+            }if(i < 12){
+                System.out.println("I is samller than 12 too");
+            }else{
+                System.out.println("I is greater than 15");
+            }
         }
     }
 }
