78a668e790171cd9def5a637f723750767d2abca
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-20 11:01:56 +0530

    Below is the implementation of the above method:

--- Stars.java
+++ Stars.java
@@ -1,8 +1,17 @@
 public class Stars{
+    public static int sum(int i, int j){
+        return i + j;
+    }
+
+    // main method 
     public static void main(String args[]){
-        int n = 5;
-        for(int i = 1; i<= n; i++){
-            System.out.println(i);
+        int n = 3, m = 3;
+
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m; j++){
+                System.out.println(sum(i, j) + " ");
+            }
+            System.out.println();
         }
     }
 }
