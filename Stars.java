8d2653c4991f11dd875db4dc1284949e4957a86b
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-20 11:06:43 +0530

    Parsing Command-line arguments

--- Stars.java
+++ Stars.java
@@ -1,17 +1,7 @@
 public class Stars{
-    public static int sum(int i, int j){
-        return i + j;
-    }
-
-    // main method 
     public static void main(String args[]){
-        int n = 3, m = 3;
-
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < m; j++){
-                System.out.println(sum(i, j) + " ");
-            }
-            System.out.println();
+        for(int i =  0; i<args.length; i++){
+            System.out.println(args[i] +  "this is just to check to");
         }
     }
 }
