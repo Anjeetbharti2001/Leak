58bfc051d8bdfa2df8a2b51458fb1a11e43e9cb4
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-21 23:49:32 +0530

    multidimension 2D arrays

--- Stars.java
+++ Stars.java
@@ -1,16 +1,14 @@
 public class Stars{
     public static void main(String args[]){
-        int[] arr = new int[5];
+        int arr[][] = {{ 1, 2, 3},{4, 5, 6},{7, 8, 9}};
 
-        // Allocating memory to the element 
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
-        arr[4] = 50;
-        
-        for(int i = 0; i<arr.length; i++){
-            System.out.println("arr[" + i + "] :" + arr[i]);
+        // printing 2D array
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println(arr[i][j] + " ");
+                System.out.println();
+            }
         }
     }
 }
