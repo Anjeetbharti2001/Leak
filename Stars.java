7422f308d21a03484ea152d8549eaadd80f21af9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-21 23:26:58 +0530

    Java Inheritance

--- Stars.java
+++ Stars.java
@@ -1,19 +1,14 @@
-import java.util.*;
-
-class ABC{
-    public int sum(int x, int y){
-        return x + y;
-    }
-    public double sum(double x , double y){
-        return x + y;
-    }
+class Employee{
+    int salary = 70000;
+}
+//Inherited or Sub Class
+class Engineer extends Employee {
+    int benefits = 15000;
 }
 
 public class Stars{
     public static void main(String args[]){
-        ABC temp = new ABC();
-
-        System.out.println(temp.sum(1, 2));
-        System.out.println(temp.sum(3.14, 4.23));
+        Engineer E1 = new Engineer();
+        System.out.println("Salary : " + E1.salary + "\nBenefits : " + E1.benefits);
     }
 }
