0261aa7a89d8b3e2759c4680a7ec655d45ef04b9
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-22 11:08:25 +0530

    Generic Functiuon

--- Stars.java
+++ Stars.java
@@ -1,14 +1,12 @@
 public class Stars{
+    static <T> void genericDisplay(T element){
+        System.out.println(element.getClass().getName() + " = " + element);
+    }
     public static void main(String args[]){
-        int arr[][] = {{ 1, 2, 3},{4, 5, 6},{7, 8, 9}};
+         genericDisplay(11);
 
-        // printing 2D array
+         genericDisplay("AnjeetBharti");
 
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                System.out.println(arr[i][j] + " ");
-                System.out.println();
-            }
-        }
+         genericDisplay(1.0);
     }
 }
