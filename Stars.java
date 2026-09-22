3eccc904053eb4623b70bd6b13a78945dd147bb6
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-23 00:21:39 +0530

    Traverse Arrays

--- Stars.java
+++ Stars.java
@@ -1,9 +1,10 @@
 public class Stars{
     public static void main(String args[]){
-        int []arr = {2,4,8,12, 16};
+        int arr[] = {2, 4, 8, 12, 16};
 
-        // Updating first element
-        arr[0] = 90;
-        System.out.print(arr[0]);
+        // Traversal and printing arrays
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
     }
 }
