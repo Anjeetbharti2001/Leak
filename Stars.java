d7ae8f0049af2e30e99f16da95752b7378621a60
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-24 11:36:53 +0530

    passing arrays method

--- Stars.java
+++ Stars.java
@@ -1,18 +1,15 @@
 public class Stars{
     public static void main(String args[]){
-        int arr[] = {3, 1, 2, 5, 4};
+        int arr[] = { 3, 1, 2, 5, 4};
 
-        // passing arrays to method m1
+        //passing arrays to methods m1
         sum(arr);
     }
-    public static void sum(int[]arr){
-        // getting sum of arrays values
+    public static void sum(int [] arr){
         int sum = 0;
-
-        for(int i = 0; i < arr.length; i++){
+        for(int i = 0; i< arr.length; i++){
             sum += arr[i];
-
-            System.out.println("Sum of arrays values : " + sum );
+            System.out.println("sum of arrays value : " + sum );
         }
     }
 }
