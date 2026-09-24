b0de3614bf7f8f1b5e44ad1253f2ac182d9a78f8
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-24 11:52:17 +0530

    return arrays methods

--- Stars.java
+++ Stars.java
@@ -1,13 +1,15 @@
 public class Stars{
+     public static int[] a1(){
+        return new int [] { 1, 5, 9};
+    }
+
     public static void main(String args[]){
 
-        int arr[] = m1();
+        int arr[] = a1();
 
         for(int i = 0; i< arr.length; i++)
             System.out.println(arr[i] + " ");
        
     }
-    public static int[] m1(){
-        return new int [] { 1, 2, 3};
-    }
+   
 }
