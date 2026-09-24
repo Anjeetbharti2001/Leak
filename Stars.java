d7a9432f662c27b3d4daee79242f8373da45af03
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-24 12:09:32 +0530

    Jagged Array in Java

--- Stars.java
+++ Stars.java
@@ -1,15 +1,28 @@
-public class Stars{
-     public static int[] a1(){
-        return new int [] { 1, 5, 9};
+
+public class Stars {
+    public static void main(String args[]) {
+
+        int arr[][] = new int[2][];
+
+        // Making the above arrays jagged
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+
+        int count = 0;
+
+        // Fill the array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count++;
+            }
+        }
+
+        // Print the array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-
-    public static void main(String args[]){
-
-        int arr[] = a1();
-
-        for(int i = 0; i< arr.length; i++)
-            System.out.println(arr[i] + " ");
-       
-    }
-   
 }
