9471a598b95f06d29bfbf2b6de64e5ddb57f2a30
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-25 11:16:30 +0530

    Create an instance of the class

--- Stars.java
+++ Stars.java
@@ -1,28 +1,11 @@
+public class Stars{
+    //An example method
+    public void printMessage(){
+        System.out.println("Anjeet Bharti");
+    }
+    public static void main(String args[]){
+        Stars obj = new Stars();
 
-public class Stars {
-    public static void main(String args[]) {
-
-        int arr[][] = new int[2][];
-
-        // Making the above arrays jagged
-        arr[0] = new int[3];
-        arr[1] = new int[2];
-
-        int count = 0;
-
-        // Fill the array
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = count++;
-            }
-        }
-
-        // Print the array
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        obj.printMessage();
     }
 }
