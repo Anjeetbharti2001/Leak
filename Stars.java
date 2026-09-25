0130fccf4aaac566285d255e9b90e6ad456bc27a
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-25 11:37:49 +0530

    Static method directry

--- Stars.java
+++ Stars.java
@@ -1,6 +1,10 @@
-public class Stars{
-    public static void main(String args[]){
-        Stars obj = new Stars();
-        System.out.println(obj.hashCode());
+  class Taste {
+    static void hello() {
+        System.out.println("Hello");
+    }
+}
+public class Stars {
+    public static void main(String[] args) {
+        Taste.hello(); // Call static method directly
     }
 }
