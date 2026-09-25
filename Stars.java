8e6f78f01159597b923744edabaa91997b9e4c41
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-26 02:23:18 +0530

    Program demonstrates a for loop that prints numbers from 0 to 10 in a single line.

--- Stars.java
+++ Stars.java
@@ -1,6 +1,6 @@
 public class Stars{
     public static void main(String args[]){
-        for(int i = 2; i <= 10 ; i += 2){
+        for(int i = 0; i<= 10; i++){
             System.out.print(i + " ");
         }
     }
