6f26e1ae1afb163c47d46e63720ecfa075d44f01
Anjeetbharti2001 <anjeetbharti2.0@gmail.com>
2026-09-25 11:31:16 +0530

    Calling the Predefined Methods

--- Stars.java
+++ Stars.java
@@ -1,9 +1,6 @@
 public class Stars{
-    void hello(){
-        System.out.println("This is user-defined method.");
-    }
     public static void main(String args[]){
         Stars obj = new Stars();
-        obj.hello();
+        System.out.println(obj.hashCode());
     }
 }
